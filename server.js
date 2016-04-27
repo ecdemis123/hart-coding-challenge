f069bc8b0208d8b4a5f9dfc662b51a1dae64cb3c
Eleanor Demis <eleanordemis@gmail.com>
2016-04-27 14:24:04 -0700

    [setup] starts server

--- server.js
+++ server.js
@@ -1,2 +1,9 @@
 'use strict'
 const express = require('express');
+const app = express();
+require('./routes.js')(app, express);
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log("http://localhost" + port)
+});
