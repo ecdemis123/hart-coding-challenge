9ddd2bea896789f384f77531a508252b01753441
Eleanor Demis <eleanordemis@gmail.com>
2016-04-27 14:37:55 -0700

    [setup] adds config information to controller

--- controller.js
+++ controller.js
@@ -1,5 +1,29 @@
 'use strict'
+const yelpUtil = require('node-yelp');
+const foursquareUtil = require('node-foursquare');
+const what3WordsUtil = require('geo.what3words');
+const config = require('./config.js');
+
+const yelpConfig = {
+  consumerKey: config.yelpoAuthConsumerKey,
+  consumerSecret: config.yelpConsumerSecret,
+  token: config.yelpoAuthToken,
+  tokenSecret: config.yelpTokenSecret
+};
+
+const foursquareConfig = {
+  clientID: config.fsClientId,
+  clientSecret: config.fsClientSecret
+};
+
+const what3WordsConfig = {
+  apiKey: config.w3wAPIKey
+};
 
 module.exports = {
-  
+  mergeRestaurants: (req, res) => {
+    
+
+  }
+
 }
